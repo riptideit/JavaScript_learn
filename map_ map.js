245d39fdee4dcabbe1165266c45b472023173a47
Fliptoss <samirman.0009@gmail.com>
2025-11-13 20:19:32 +0600

    map

---
+++ map_ map.js
@@ -0,0 +1,14 @@
+/*
+1. key types
+2. key order
+3. size
+4. iteration
+5. performance (insertion and deletion)
+*/
+
+const newMap = new Map();
+
+newMap.set(10, "values will be here");
+console.log(newMap.get(10));
+
+console.log(newMap.keys(), newMap.values());
